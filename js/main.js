36360ae776a53e931e2b43040562a4e4fcc11cbc
Mahmoud Elgamal <elgamal.mem@gmail.com>
2019-12-15 22:40:13 +0200

    header animation

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-const btnCollapse = document.querySelector('.collapse-btn')
-const navigation__nav = document.querySelector('.navigation__nav')
-
-btnCollapse.addEventListener('click', () => {
-    navigation__nav.classList.toggle('open')
-})
-
-// smoothe scroll
-var scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 500,
-    speedAsDuration: true,
-});
-
-// typed
-document.addEventListener("DOMContentLoaded", function () {
+$( document ).ready(function() {
+    const btnCollapse = document.querySelector('.collapse-btn')
+    const navigation__nav = document.querySelector('.navigation__nav')
+    
+    btnCollapse.addEventListener('click', () => {
+        navigation__nav.classList.toggle('open')
+    })
+    
+    // smoothe scroll
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 500,
+        speedAsDuration: true,
+    });
+    
+    // typed
     var typed = new Typed('#typed', {
         stringsElement: '#typed-strings',
         cursorChar: '',
@@ -23,7 +23,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var wow = new WOW({
         boxClass: 'wow', // animated element css class (default is wow)
         animateClass: 'animated', // animation css class (default is animated)
-        offset: 0, // distance to the element when triggering the animation (default is 0)
         mobile: true, // trigger animations on mobile devices (default is true)
         live: true, // act on asynchronously loaded content (default is true)
         callback: function (box) {
