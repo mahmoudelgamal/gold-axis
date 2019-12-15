16a311fad8b866940d0cc98abbcf2cfde8322105
Mahmoud Elgamal <elgamal.mem@gmail.com>
2019-12-15 21:18:52 +0200

    solutions section

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ const navigation__nav = document.querySelector('.navigation__nav')
 btnCollapse.addEventListener('click', () => {
     navigation__nav.classList.toggle('open')
 })
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 500,
+	speedAsDuration: true
+});
