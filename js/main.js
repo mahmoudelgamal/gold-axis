5475cc7f0ea129ba37f85dc89a3b29610e03a008
Mahmoud Elgamal <elgamal.mem@gmail.com>
2019-12-15 22:31:08 +0200

    header animation

--- js/main.js
+++ js/main.js
@@ -5,8 +5,32 @@ btnCollapse.addEventListener('click', () => {
     navigation__nav.classList.toggle('open')
 })
 
+// smoothe scroll
 var scroll = new SmoothScroll('a[href*="#"]', {
-	speed: 500,
+    speed: 500,
     speedAsDuration: true,
-    
 });
+
+// typed
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        cursorChar: '',
+        typeSpeed: 20
+
+    });
+});
+
+var wow = new WOW({
+    boxClass: 'wow', // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: true, // trigger animations on mobile devices (default is true)
+    live: true, // act on asynchronously loaded content (default is true)
+    callback: function (box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null // optional scroll container selector, otherwise use window
+});
+wow.init();
